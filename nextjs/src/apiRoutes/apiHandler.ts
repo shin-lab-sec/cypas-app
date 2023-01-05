f5eb105600a9491b080542428337f2de1efb02d9
t-kabu <t.kabu.03@gmail.com>
2023-01-05 20:34:41 +0900

    feat: デフォルトでapiHandlerに認証確認をはさむ

--- nextjs/src/apiRoutes/apiHandler.ts
+++ nextjs/src/apiRoutes/apiHandler.ts
@@ -1,4 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Session } from 'next-auth'
+import { getSession } from 'next-auth/react'
 import { Api } from 'types/apiSchema'
 
 type UrlMap =
@@ -20,36 +22,49 @@ export const apiHandler =
           Url extends keyof Api['GET'] ? Api['GET'][Url][0] : undefined
         >,
         res: ApiResponse<
-          Url extends keyof Api['GET'] ? Api['GET'][Url][1] : undefined
+          Url extends keyof Api['GET'] ? Api['GET'][Url][1] | Error : undefined
         >,
+        session: Session,
       ) => void
       post?: (
         req: ApiRequest<
           Url extends keyof Api['POST'] ? Api['POST'][Url][0] : undefined
         >,
         res: ApiResponse<
-          Url extends keyof Api['POST'] ? Api['POST'][Url][1] : undefined
+          Url extends keyof Api['POST']
+            ? Api['POST'][Url][1] | Error
+            : undefined
         >,
+        session: Session,
       ) => void
       put?: (
         req: ApiRequest<
           Url extends keyof Api['PUT'] ? Api['PUT'][Url][0] : undefined
         >,
         res: ApiResponse<
-          Url extends keyof Api['PUT'] ? Api['PUT'][Url][1] : undefined
+          Url extends keyof Api['PUT'] ? Api['PUT'][Url][1] | Error : undefined
         >,
+        session: Session,
       ) => void
       delete?: (
         req: ApiRequest<
           Url extends keyof Api['DELETE'] ? Api['DELETE'][Url][0] : undefined
         >,
         res: ApiResponse<
-          Url extends keyof Api['DELETE'] ? Api['DELETE'][Url][1] : undefined
+          Url extends keyof Api['DELETE']
+            ? Api['DELETE'][Url][1] | Error
+            : undefined
         >,
+        session: Session,
       ) => void
     },
   ) =>
-  (req: NextApiRequest, res: NextApiResponse) => {
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    const session = await getSession({ req })
+    if (!session) {
+      return res.status(403).json({ message: 'unauthorized error' })
+    }
+
     switch (req.method) {
       case 'GET':
         handlers.get?.(
@@ -58,16 +73,17 @@ export const apiHandler =
             Url extends keyof Api['GET'] ? Api['GET'][Url][0] : undefined
           >,
           res,
+          session,
         )
         break
       case 'POST':
-        handlers.post?.(req, res)
+        handlers.post?.(req, res, session)
         break
       case 'PUT':
-        handlers.put?.(req, res)
+        handlers.put?.(req, res, session)
         break
       case 'DELETE':
-        handlers.delete?.(req, res)
+        handlers.delete?.(req, res, session)
         break
       default:
         res.status(405).json({ message: 'unknown method' })
