1de327633b911eed0cc7fafcd7ac279070dc109b
t-kabu <t.kabu.03@gmail.com>
2023-05-29 00:20:53 +0900

    feat: useAppState実装

---
+++ nextjs/src/foundation/appState.ts
@@ -0,0 +1,20 @@
+import useSWR, { KeyedMutator } from 'swr'
+
+type AppState = {
+  openNavbar: boolean
+}
+
+const initState: AppState = {
+  openNavbar: true,
+}
+
+export const useAppState = <Path extends keyof AppState>(
+  path: Path,
+): [AppState[Path], KeyedMutator<AppState[Path]>] => {
+  const { data, mutate } = useSWR<AppState[Path]>(path, null, {
+    fallbackData: initState[path],
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  })
+  return [data as AppState[Path], mutate]
+}
